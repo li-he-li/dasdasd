4d7afefc2a2b7f16573366c4665bc3935dfe053a
li-he-li <li515682241@outlook.com>
2025-09-27 16:28:24 +0800

    Delete assignment2_homework directory

--- assignment2_homework/centre.py
+++
@@ -1,9 +0,0 @@
-from skimage import io
-image = io.imread('x.jpg')
-(h,w,c)=image.shape
-print(h,w,c)
-for i in range(3,7):
-    for j in range(3,7):
-        image[i, j, 0] = 255
-io.imshow(image)
-io.show()
--- assignment2_homework/sfzi.py
+++
@@ -1,9 +0,0 @@
-from skimage import io
-image = io.imread('x.jpg')
-(h,w,c)=image.shape
-print(h,w,c)
-for i in range(h//2):
-    for j in range(w//2):
-        image[i, j, 0] = 255
-io.imshow(image)
-io.show()
